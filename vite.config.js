743a07d455c72f0d0221a7df3ab8d50d37c1962f
mutemi1998 <kenmuthui65@gmail.com>
2023-06-12 11:36:35 +0300

    Fixed path bug when building

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,14 @@
-import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from "node:url";
 
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json', '.vue', '.mjs', '.wasm', '.css', '.asset'],
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
 });
